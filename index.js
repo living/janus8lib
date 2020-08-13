e307de041c74650303e279f9cbc36dba55e43aa2
marcioxrocha <marcioxrocha@gmail.com>
2020-08-12 23:59:51 -0300

    faltou

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const { Description } = require('./lib/mongoose/custom.types');
-const { ProcessMessages } = require('./lib/mongoose/process.messages')
+const { Description, YesOrNo, Enum } = require('./lib/mongoose/custom.types');
+const { ProcessMessages } = require('./lib/mongoose/process.messages');
+const { Util } = require('./lib/util')
 
 const janus8lib = {
     customTypes: {
         mongoose: {
             Description: Description,
+            YesOrNo: YesOrNo,
+            Enum: Enum
         }
     },
-    ProcessMessages: ProcessMessages
+    ProcessMessages: ProcessMessages,
+    Util = Util
 }
 
 module['exports'] = janus8lib;
