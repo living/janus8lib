18a7e51de515a4f7873a7f8067c8b17fcd2ec8c0
marcioxrocha <marcioxrocha@gmail.com>
2020-08-14 15:29:19 -0300

    trabalhando

--- test/test.js
+++ test/test.js
@@ -1,15 +1,18 @@
 /* eslint-env mocha */
 ;(function() {
     const root = require('..');
-    const should = require('should')
+    const should = require('should');
+    const validator = require('validator');
   
     // Tests
     // ------------------------------------------------------------
     describe('Util:', () => {
-        it('transformValues', done => {
+        it('transformValues:', done => {
+            root.Util.transformValues("nao", ["Sim","Não"]).should.equal("Não");
+            root.Util.transformValues("não", ["Sim","Não"]).should.equal("Não");
             root.Util.transformValues("sim/Teste", ["Sim","Não"]).should.equal("Sim");
             return done()
-        })
+        });
     })
 })()
   
