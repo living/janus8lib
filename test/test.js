dc8a8caf5e19c7552f495ec6827db5b16c5f65c9
marcioxrocha <marcioxrocha@gmail.com>
2020-08-27 11:40:38 -0300

    correcao

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@
     // ------------------------------------------------------------
     describe('Util:', () => {
         it('transformValues:', done => {
+            validator.isIn(root.Util.transformValues("nao", ["Sim","Não"]), ["Sim","Não"]).should.equal(true);
             root.Util.transformValues("nao", ["Sim","Não"]).should.equal("Não");
             root.Util.transformValues("não", ["Sim","Não"]).should.equal("Não");
             root.Util.transformValues("sim/Teste", ["Sim","Não"]).should.equal("Sim");
