d45ce21b9008510b9e41d2560e8c7b9baa53ffca
marcioxrocha <marcioxrocha@gmail.com>
2020-09-04 18:35:15 -0300

    toDate

--- test/test.js
+++ test/test.js
@@ -14,6 +14,20 @@
             root.Util.transformValues("sim/Teste", ["Sim","Não"]).should.equal("Sim");
             return done()
         });
+        it('toDate:', done => {
+            var date = new Date('2020-09-04T00:00:00-03:00');
+
+            var val = root.Util.toDate('2020-09-04');
+            (val?val.getTime():undefined).should.equal(date.getTime());
+
+            var val = root.Util.toDate('04-09-2020');
+            (val?val.getTime():undefined).should.equal(date.getTime());
+
+            var val = root.Util.toDate('04-09-2020 00:00:00');
+            (val?val.getTime():undefined).should.equal(date.getTime());            
+
+            return done()
+        });
     })
 })()
   
