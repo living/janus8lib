71b0f449b764a4f15e2bf22ebb1fc8a612d96d6c
marcioxrocha <marcioxrocha@gmail.com>
2021-01-28 14:43:23 -0300

    correcao

--- test/test.js
+++ test/test.js
@@ -25,6 +25,9 @@
             var val = root.Util.toDate(new Date('2020-09-08T00:00:00Z'));
             (val?val.getTime():undefined).should.equal(date.getTime());
 
+            var val = root.Util.toDate('2020-09-08T00:00:00-03:00');
+            (val?val.getTime():undefined).should.equal(date.getTime());            
+
             var t1 = '2020-09-08';
 
             var val = root.Util.toDate(t1);
